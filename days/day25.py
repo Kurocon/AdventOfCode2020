40ae8e604eb0e3bc0967c220cf868a8194769a6b
Kevin Alberts <kevin@kevinalberts.nl>
2020-12-25 06:49:16 +0100

    Added optimized day 25 solution

--- days/day25.py
+++ days/day25.py
@@ -12,23 +12,29 @@ class Day25(AOCDay):
         # input_data = self.test_input
         self.card_pubkey, self.door_pubkey = map(int, input_data)
 
-    def part1(self, input_data):
-        def transform(subject_number, loop_size):
-            # Python pow has a modulo parameter
-            return pow(subject_number, loop_size, 20201227)
+    def get_loop_size(self, subject_number, pub_key):
+        loop_size = 0
+        value = 1
+        while value != pub_key:
+            loop_size += 1
+            value = (value * subject_number) % 20201227
+        return loop_size
 
-        card_loop_size = 0
-        while transform(7, card_loop_size) != self.card_pubkey:
-            card_loop_size += 1
+    def get_encryption_key(self, subject_number, loop_size):
+        value = 1
+        for _ in range(loop_size):
+            value = (value * subject_number) % 20201227
+        return value
+
+    def part1(self, input_data):
+        card_loop_size = self.get_loop_size(7, self.card_pubkey)
         self.debug(f"Card loop size: {card_loop_size}")
 
-        door_loop_size = 0
-        while transform(7, door_loop_size) != self.door_pubkey:
-            door_loop_size += 1
+        door_loop_size = self.get_loop_size(7, self.door_pubkey)
         self.debug(f"Door loop size: {door_loop_size}")
 
-        encryption_key = transform(self.card_pubkey, door_loop_size)
-        assert encryption_key == transform(self.door_pubkey, card_loop_size)
+        encryption_key = self.get_encryption_key(self.card_pubkey, door_loop_size)
+        assert encryption_key == self.get_encryption_key(self.door_pubkey, card_loop_size)
         self.debug(f"Encryption key: {encryption_key}")
         yield encryption_key
 
